8ab691ef50adec5276e1d9afd799ad4440805d1d
Amina <mamadou@MacBook-Pro-de-Amina.local>
2024-12-05 11:36:34 +0000

    four commit

--- Program.cs
+++ Program.cs
@@ -20,4 +20,10 @@ c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 app.MapGet("/", () => "Bonjour Sénégal!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+await db.Pizzas.AddAsync(pizza);
+await db.SaveChangesAsync();
+return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 app.Run();
